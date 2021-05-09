79568d2f42d8317cf091256fb26b3eb084f477df
abhisheksaral <abhisheksaral17@gmail.com>
2021-05-09 02:14:41 -0700

    Initial Commit

---
+++ src/main/java/com/dailycodebuffer/config/DynamoDbConfiguration.java
@@ -0,0 +1,40 @@
+package com.dailycodebuffer.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbConfiguration {
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "dynamodb.us-east-1.amazonaws.com",
+                                "us-east-1"
+                        )
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(
+                                        "AKIAWXDUL3H6MI5KDXNU",
+                                        "FWeCod0Q9OJ99+C//MPyI6IsGWN6jCLvWKS4reXd"
+                                )
+                        )
+                )
+                .build();
+    }
+
+}
